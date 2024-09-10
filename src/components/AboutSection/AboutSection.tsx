909b2bb8c7bd110a03cdc26a7bd87ab6ffebcd79
ankkitsharma <ankitsharma2801@gmail.com>
2024-09-10 21:03:06 +0530

    add about body

--- src/components/AboutSection/AboutSection.tsx
+++ src/components/AboutSection/AboutSection.tsx
@@ -5,6 +5,20 @@ export default function AboutSection() {
   return (
     <div className="h-full flex flex-col">
       <ButtonContainer />
+      <AboutBody />
+    </div>
+  );
+}
+
+function AboutBody() {
+  return (
+    <div className="text-xl leading-7 text-[#969696]">
+      Hello! I&apos;m Dave, your sales rep here from Salesforce. I&apos;ve been
+      working at this awesome company for 3 years now. I was born and raised in
+      Albany, NY& have been living in Santa Carla for the past 10 years my wife
+      Tiffany and my 4 year old twin daughters- Emma and Ella. Both of them are
+      just starting school, so my calender is usually blocked between 9-10 AM.
+      This is a...
     </div>
   );
 }
--- src/components/AboutSection/ButtonContainer.tsx
+++ src/components/AboutSection/ButtonContainer.tsx
@@ -1,17 +1,23 @@
+"use client";
 import styles from "./AboutSection.module.css";
+import { useState } from "react";
 
 export default function ButtonContainer() {
+  const [activeBtn, setactiveBtn] = useState("");
+  function onClick() {}
   return (
     <div className="w-full min-h-9 h-[20%] bg-[#171717] rounded-[23px] p-2 flex justify-between items-center gap-2">
-      <Button body="About Me" />
-      <Button body="Experiences" />
-      <Button body="Recommended" />
+      <Button body="About Me" id="btn1" />
+      <Button body="Experiences" id="btn2" />
+      <Button body="Recommended" id="btn3" />
     </div>
   );
 }
 
-function Button({ body }: { body: string }) {
+function Button({ body, id }: { body: string; id: string }) {
   return (
-    <button className={`w-1/3 h-full relative ${styles.btn}`}>{body}</button>
+    <button className={`w-1/3 h-full relative rounded-2xl ${styles.btn} z-[1]`}>
+      {body}
+    </button>
   );
 }
