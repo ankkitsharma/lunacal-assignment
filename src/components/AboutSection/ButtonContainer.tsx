2a7c727cbe89b729c66fac43d71e1d3bb3159473
ankkitsharma <ankitsharma2801@gmail.com>
2024-09-11 12:11:15 +0530

    Finish Widget1

--- src/components/AboutSection/ButtonContainer.tsx
+++ src/components/AboutSection/ButtonContainer.tsx
@@ -1,22 +1,72 @@
 "use client";
+import { cn } from "@/lib/utils";
 import styles from "./AboutSection.module.css";
 import { useState } from "react";
 
 export default function ButtonContainer() {
-  const [activeBtn, setactiveBtn] = useState("");
-  function onClick() {}
+  const [activeBtn, setactiveBtn] = useState("btn1");
+  function handleClick(id: string, e: EventTarget) {
+    const el = e;
+    const prevEl = document.querySelector(`#${activeBtn}`);
+    if (el instanceof HTMLElement) {
+      const x =
+        prevEl == null
+          ? "0%"
+          : prevEl.getBoundingClientRect().x -
+            el.getBoundingClientRect().x +
+            "px";
+      el.style.setProperty("--x", x);
+    }
+    setactiveBtn(id);
+  }
   return (
-    <div className="w-full min-h-9 h-[20%] bg-[#171717] rounded-[23px] p-2 flex justify-between items-center gap-2">
-      <Button body="About Me" id="btn1" />
-      <Button body="Experiences" id="btn2" />
-      <Button body="Recommended" id="btn3" />
+    <div className="w-full min-h-9 h-[20%] bg-[#171717] rounded-[23px] p-2 flex justify-between items-center gap-2 mb-[35px]">
+      <Button
+        body="About Me"
+        id="btn1"
+        handleClick={handleClick}
+        activeBtn={activeBtn}
+      />
+      <Button
+        body="Experiences"
+        id="btn2"
+        handleClick={handleClick}
+        activeBtn={activeBtn}
+      />
+      <Button
+        body="Recommended"
+        id="btn3"
+        handleClick={handleClick}
+        activeBtn={activeBtn}
+      />
     </div>
   );
 }
 
-function Button({ body, id }: { body: string; id: string }) {
+function Button({
+  body,
+  id,
+  handleClick,
+  activeBtn,
+}: {
+  body: string;
+  id: string;
+  handleClick: (id: string, e: EventTarget) => void;
+  activeBtn: string;
+}) {
   return (
-    <button className={`w-1/3 h-full relative rounded-2xl ${styles.btn} z-[1]`}>
+    <button
+      className={cn(
+        "w-1/3 h-full relative rounded-2xl z-[1]",
+        styles.btn,
+        activeBtn === id && styles.active,
+        activeBtn !== id && styles.notActive
+      )}
+      id={id}
+      onClick={(e) => {
+        handleClick(id, e.target);
+      }}
+    >
       {body}
     </button>
   );
--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,6 +1,6 @@
 export default function Widget({ body }: { body?: JSX.Element }) {
   return (
-    <div className="w-full h-full relative rounded-[18.89px] px-14 pt-5 bgFrame">
+    <div className="w-full h-full relative rounded-[18.89px] px-14 pt-5 pb-[1.23rem] bgFrame">
       {body}
       <div
         className={`absolute -bottom-[21px] left-1/2 -translate-x-1/2 w-[70%] h-1 rounded-[2.46px] hr`}
